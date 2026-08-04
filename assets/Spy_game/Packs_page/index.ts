644e68a751479274f49edc2fda280c39ccf9d996
Stolzie071 <96483539+Stolzie071@users.noreply.github.com>
2026-08-04 21:06:30 +0300

    feat(spy): export Marvel and DC pack illustrations

--- assets/Spy_game/Packs_page/index.ts
+++ assets/Spy_game/Packs_page/index.ts
@@ -1,5 +1,7 @@
 export { default as CitiesPackIllustration } from "./Cities.svg";
+export { default as DcHeroesPackIllustration } from "./dc_heroes.svg";
 export { default as Dota2HeroesPackIllustration } from "./dota2_heroes.svg";
+export { default as MarvelHeroesPackIllustration } from "./marvel_heroes.svg";
 export { default as NaturePackIllustration } from "./Nature.svg";
 export { default as TransportPackIllustration } from "./Transport.svg";
 export { default as WorkplacesPackIllustration } from "./Working_place.svg";
