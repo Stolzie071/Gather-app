f98fa010fae1ab459b85c884787eb339364de1a9
Stolzie071 <96483539+Stolzie071@users.noreply.github.com>
2026-08-04 18:24:05 +0300

    feat(spy): export Dota heroes pack illustration

--- assets/Spy_game/Packs_page/index.ts
+++ assets/Spy_game/Packs_page/index.ts
@@ -1,4 +1,5 @@
 export { default as CitiesPackIllustration } from "./Cities.svg";
+export { default as Dota2HeroesPackIllustration } from "./dota2_heroes.svg";
 export { default as NaturePackIllustration } from "./Nature.svg";
 export { default as TransportPackIllustration } from "./Transport.svg";
 export { default as WorkplacesPackIllustration } from "./Working_place.svg";
