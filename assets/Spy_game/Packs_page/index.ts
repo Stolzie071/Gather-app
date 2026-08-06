48dd1eb8ff6ed1384fa539b707da267ff10075d7
Stolzie071 <96483539+Stolzie071@users.noreply.github.com>
2026-08-06 19:14:17 +0300

    feat(spy): export animal pack illustrations

--- assets/Spy_game/Packs_page/index.ts
+++ assets/Spy_game/Packs_page/index.ts
@@ -1,7 +1,9 @@
 export { default as CitiesPackIllustration } from "./Cities.svg";
 export { default as DcHeroesPackIllustration } from "./dc_heroes.svg";
+export { default as DomesticAnimalsPackIllustration } from "./pets.svg";
 export { default as Dota2HeroesPackIllustration } from "./dota2_heroes.svg";
 export { default as MarvelHeroesPackIllustration } from "./marvel_heroes.svg";
 export { default as NaturePackIllustration } from "./Nature.svg";
 export { default as TransportPackIllustration } from "./Transport.svg";
+export { default as WildAnimalsPackIllustration } from "./wild_animals.svg";
 export { default as WorkplacesPackIllustration } from "./Working_place.svg";
