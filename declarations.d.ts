5ae6fc2cf9692305fe329c8c496a81ba46d75a53
Stolzie071 <antonl03@yandex.ru>
2026-08-26 00:17:43 +0300

    perf: optimize location assets and Android release size

--- declarations.d.ts
+++ declarations.d.ts
@@ -10,3 +10,13 @@ declare module "*.png" {
   const content: number;
   export default content;
 }
+
+declare module "*.jpg" {
+  const content: number;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: number;
+  export default content;
+}
